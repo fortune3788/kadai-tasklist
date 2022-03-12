5d805c8a8c78968308ae1fdef9b8c14d5d8b616d
fortune <basket85r@gmail.com>
2022-03-12 10:55:35 +0000

    add column  validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_090412) do
+ActiveRecord::Schema.define(version: 2022_03_12_094328) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
