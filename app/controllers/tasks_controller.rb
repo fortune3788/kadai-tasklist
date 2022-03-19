f8bac88c72b8ff271de1afd1b7e6ad15df713eee
fortune <basket85r@gmail.com>
2022-03-19 14:36:33 +0000

    modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,58 +1,27 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
   
-  def index
-    if logged_in?
-      @task = current_user.tasks.build
-      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
-    end
-  end
-  
-  def show
-  end
-  
-  def new
-    @task = Task.new
-  end
   
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Taskが正常に投稿されました'
+      flash[:success] = 'タスクが正常に投稿されました'
       redirect_to root_url
     else
       @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
-      flash.now[:danger] = 'Taskが投稿されませんでした'
-      render 'tasks/index'
-    end
-  end
-  
-  def edit
-  end
-  
-  def update
-    if @task.update(task_params)
-      flash[:success] = 'Taskは正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Taskは更新されませんでした'
-      render :edit
+      flash.now[:danger] = 'タスクが投稿されませんでした'
+      render 'toppages/index'
     end
   end
   
   def destroy
     @task.destroy
-    flash[:success] = 'Taskは正常に削除されました'
+    flash[:success] = 'タスクは正常に削除されました'
     redirect_back(fallback_location:root_path)
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   #Storong Parameter
   def task_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,4 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  
-  def index
-    @pagy, @users = pagy(User.order(id: :desc), items: 15)
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @pagy, @tasks = pagy(@user.tasks.order(id: :desc))
-    counts(@user)
-  end
 
   def new
     @user = User.new
@@ -20,7 +9,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザ登録に失敗しました'
       render :new
