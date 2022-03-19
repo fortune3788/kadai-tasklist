bfc0c12acc341815ffe731b4fa0da2d9aa28956f
fortune <basket85r@gmail.com>
2022-03-19 02:35:15 +0000

    login　function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
   end
   
   def show
@@ -13,14 +18,14 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
       flash.now[:danger] = 'Taskが投稿されませんでした'
-      render :new
+      render 'tasks/index'
     end
   end
   
@@ -39,9 +44,8 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location:root_path)
   end
   
   private
@@ -53,5 +57,12 @@ class TasksController < ApplicationController
   #Storong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
