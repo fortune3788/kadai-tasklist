4e24da7cef8e2bfe9c251c794c8ec0555c2ac77b
fortune <basket85r@gmail.com>
2022-03-23 15:29:02 +0000

    modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,21 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update]
+  before_action :require_user_logged_in, only: [:create, :destroy]
   before_action :correct_user, only: [:destroy]
   
+  def index
+    if logged_in?
+      @task = current_user.tasks.build
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
   
   def create
     @task = current_user.tasks.build(task_params)
@@ -11,19 +25,35 @@ class TasksController < ApplicationController
     else
       @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
       flash.now[:danger] = 'タスクが投稿されませんでした'
-      render 'toppages/index'
+      render 'tasks/index'
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Taskは正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskは更新されませんでした'
+      render :edit
     end
   end
   
   def destroy
     @task.destroy
     flash[:success] = 'タスクは正常に削除されました'
-    redirect_back(fallback_location:root_path)
+    redirect_to root_url
   end
   
   private
   
-  #Storong Parameter
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
     params.require(:task).permit(:content, :status)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'toppages#index'
+  root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:create]
   
-  resources :tasks, only: [:create, :destroy]
+  resources :tasks
 end
